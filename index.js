6158266585f7a16d580930531de0e7535de6ca2d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-15 10:48:34 +0430

    adding some changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const navToggle = document.querySelector(".nav-toggle");
+const navList = document.querySelectorAll(".nav-list");
 
 navToggle.addEventListener("click", function () {
   document.body.classList.toggle("nav-open");
